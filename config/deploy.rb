42963b11750b9a08bbd69f2684fd25f59f5c4a47
tka.lu <tkatka@gmail.com>
2012-12-01 14:04:01 +0800

    update Gemfile

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 default_environment["PATH"] = "/opt/ruby/bin:/usr/local/bin:/usr/bin:/bin"
 
 set :application, "pcc-web"
-set :repository,  "git@github.com:example/#{application}.git"
+set :repository,  "git@github.com:tka/pcc-web.git"
 set :deploy_to, "/home/apps/#{application}"
 
 set :branch, "master"
