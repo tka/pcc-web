e9a808e794e2c1c0ffb0294c9f4ac72e8b27865d
tka.lu <tkatka@gmail.com>
2012-12-12 23:20:45 +0800

    add tenderer view

--- app/controllers/tenderers_controller.rb
+++ app/controllers/tenderers_controller.rb
@@ -2,4 +2,9 @@ class TenderersController < ApplicationController
   def index
     @tenderers = Tenderer.order(:id).paginate(:page => params[:page])
   end
+  def show
+    @tenderer = Tenderer.find(params[:id])
+    @winning_tender_infos = @tenderer.tender_infos.includes(:procurement).where(:winning => true)
+    @lose_tender_infos = @tenderer.tender_infos.includes(:procurement).where(:winning => false)
+  end
 end
