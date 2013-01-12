7efe6152bd55afd0470119605fe9d97f51f1d9b6
tka.lu <tkatka@gmail.com>
2013-01-12 09:17:05 +0800

    add winning_tender_info_count

---
+++ db/migrate/20121212170942_add_tender_info_counts.rb
@@ -0,0 +1,15 @@
+class AddTenderInfoCounts < ActiveRecord::Migration
+  def up
+    add_column :tenderers, :tender_info_count, :integer, :deault => 0
+    add_column :tenderers, :winning_tender_info_count, :integer, :deault => 0
+    add_index :tenderers, :tender_info_count
+    add_index :tenderers, :winning_tender_info_count
+    ActiveRecord::Base.connection.execute('update tenderers set tender_info_count = (select count(*) from tender_infos where tender_infos.tenderer_id = tenderers.id)')
+    ActiveRecord::Base.connection.execute('update tenderers set winning_tender_info_count = (select count(*) from tender_infos where tender_infos.tenderer_id = tenderers.id and winning = 1)')
+  end
+
+  def down
+    remove_column :tenderers, :tender_info_count
+    remove_column :tenderers, :winning_tender_info_count
+  end
+end
