7d35021d5850b378a513c4e0c881566a9362496a
tka.lu <tkatka@gmail.com>
2013-01-27 13:53:58 +0800

    tenderers_controller json

--- app/controllers/tenderers_controller.rb
+++ app/controllers/tenderers_controller.rb
@@ -3,9 +3,17 @@ class TenderersController < ApplicationController
     @q = Tenderer.search(params[:q])
     @tenderers = @q.result.paginate(:page => params[:page])
   end
+
   def show
     @tenderer = Tenderer.find(params[:id])
     @winning_tender_infos = @tenderer.tender_infos.includes(:procurement).where(:winning => true)
     @lose_tender_infos = @tenderer.tender_infos.includes(:procurement).where(:winning => false)
+    
+    respond_to do |format|
+      format.html 
+      format.json {
+        render :json => @tenderer
+      }
+    end
   end
 end
