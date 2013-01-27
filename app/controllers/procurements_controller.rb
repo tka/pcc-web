e3f68d0d091d187320a90b21a1b455dd35928eeb
tka.lu <tkatka@gmail.com>
2013-01-27 17:11:30 +0800

    procurements/show

--- app/controllers/procurements_controller.rb
+++ app/controllers/procurements_controller.rb
@@ -4,6 +4,32 @@ class ProcurementsController < ApplicationController
     @procurements = Procurement.includes(:procuring_entity).order(:finish_at).paginate(:page => params[:page])
   end
 
+  def show
+    @procurement = Procurement.includes(:procuring_entity).find(params[:id])
+    uri     = URI("http://localhost:9200/g0v/procurement/_search")
+    http    = Net::HTTP.new(uri.host, uri.port)
+    request = Net::HTTP::Post.new(uri.request_uri)
+    @query_string = '{
+    "query": {
+      "bool": {
+      "must": [
+        {"query_string": {"default_field": "標案案號", "query": "'+ @procurement.job_number+ '" } }
+      ]
+    }
+    },
+    "size": 10
+    }'
+    request.body = @query_string
+    
+    respone = http.request(request)
+    @json = JSON.load(respone.body)["hits"]["hits"].first["_source"]
+    respond_to do |format|
+      format.html 
+      format.json { send_data @json, :type => "text/json", :disposition => 'inline' }
+    end
+
+  end
+
   def complex_search
     uri     = URI("http://localhost:9200/g0v/procurement/_search")
     http    = Net::HTTP.new(uri.host, uri.port)
