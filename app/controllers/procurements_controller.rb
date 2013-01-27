0914854581bd41c32b45a35a1bfd4f3308941de7
tka.lu <tkatka@gmail.com>
2013-01-27 15:01:27 +0800

    update form

--- app/controllers/procurements_controller.rb
+++ app/controllers/procurements_controller.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 class ProcurementsController < ApplicationController
   def index
     @procurements = Procurement.includes(:procuring_entity).order(:finish_at).paginate(:page => params[:page])
@@ -7,7 +8,24 @@ class ProcurementsController < ApplicationController
     uri     = URI("http://localhost:9200/g0v/procurement/_search")
     http    = Net::HTTP.new(uri.host, uri.port)
     request = Net::HTTP::Post.new(uri.request_uri)
-    @query_string =  params[:query_string] || "{}"
+    @query_string =  params[:query_string] || '{
+    "query": {
+      "bool": {
+      "must": [
+        {
+      "query_string": {
+      "default_field": "廠商名稱",
+    "query": "台灣"
+    }
+    }
+    ]
+    }
+    },
+    "from": 0,
+    "size": 50,
+    "sort": [ ],
+    "facets": { }
+    }'
     request.body = @query_string
     
     respone = http.request(request)
